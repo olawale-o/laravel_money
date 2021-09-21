694009763507781c5aa29480300e36c653d01df7
olawale-o <omoogunolawale@gmail.com>
2021-09-21 17:04:02 +0100

    Register Laravel\Passport\PassportServiceProvider::class in app.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use App\Providers\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
